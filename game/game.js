c96bac0bd0040272ba28a66720872f8602426df4
venti9 <1dashanyasha1@mail.ru>
2022-02-12 15:46:43 +0300

    Update game.js

--- game/game.js
+++ game/game.js
@@ -44,8 +44,6 @@ function addPlatforms() {
   platforms.create(300, 450, 'platform2');
   platforms.create(400, 350, 'platform2');
   platforms.create(100, 100, 'platform2');
-  platforms.create(100, 300, 'platform');
-  platforms.create(300, 350, 'platform');
   platforms.setAll('body.immovable', true);
 }
 
